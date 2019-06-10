ae58599613229a5a655f2032696e0cd072166ea6
Björn Jansson <bjornjansson@Bjorns-iMac.local>
2019-06-10 12:16:02 +0200

    mongodb connected-postman contacts added to DB

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
-
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
@@ -6,13 +5,7 @@ const db = config.get('mongoURI');
 const connectDB = async () => {
 	try {
 		await mongoose.connect(db, {
-            _useNewUrlParser: true,
-            get useNewUrlParser() {
-                return this._useNewUrlParser;
-            },
-            set useNewUrlParser(value) {
-                this._useNewUrlParser = value;
-            },
+			useNewUrlParser: true,
 			useCreateIndex: true,
 			useFindAndModify: false
 		});
@@ -26,3 +19,35 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+
+
+
+
+// const mongoose = require('mongoose');
+// const config = require('config');
+// const db = config.get('mongoURI');
+
+// const connectDB = async () => {
+// 	try {
+// 		await mongoose.connect(db, {
+//             _useNewUrlParser: true,
+//             get useNewUrlParser() {
+//                 return this._useNewUrlParser;
+//             },
+//             set useNewUrlParser(value) {
+//                 this._useNewUrlParser = value;
+//             },
+// 			useCreateIndex: true,
+// 			useFindAndModify: false
+// 		});
+
+// 		console.log('MongoDB Connected...');
+// 	} catch (err) {
+// 		console.error(err.message);
+// 		// Exit process with failure
+// 		process.exit(1);
+// 	}
+// };
+
+// module.exports = connectDB;
